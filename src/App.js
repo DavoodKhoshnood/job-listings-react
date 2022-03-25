cfd5ad407b4afae1fd70a7d29282b53bba3224e3
Davood Khoshnood <45537769+DavoodKhoshnood@users.noreply.github.com>
2022-03-25 23:58:56 +0000

    Level 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import './App.css';
+import './App.css'
+import  HeaderBackground from './images/bg-header-desktop.svg'
+import cardImage from './images/myhome.svg';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
-      
+         <img src={HeaderBackground} alt= 'Header image' />
       </header>
+      <div className='main'>
+        <div className='card'>
+          <img src={cardImage} alt='image' />
+          <div className='description'>
+            <span className='sk-1'>Photosnap</span>
+            <span className='sk-2'>NEW!</span>
+            <span className='sk-3'>FEATURED</span><br/> 
+            <span className='sk-title'>Senior Frontend Developer</span><br/> 
+            <span className='bt'>Id ago</span>
+            <span className='bt'>.</span>
+            <span className='bt'>Full Time</span>
+            <span className='bt'>.</span>
+            <span className='bt'>USA only</span>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
