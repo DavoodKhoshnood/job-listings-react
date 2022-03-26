83d1f8b0b3ca8ae91895da90dcb1dca13f026bcd
Davood Khoshnood <45537769+DavoodKhoshnood@users.noreply.github.com>
2022-03-26 01:18:57 +0000

    Start!

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 import './App.css'
-import  HeaderBackground from './images/bg-header-desktop.svg'
-import cardImage from './images/myhome.svg';
+import HeaderBackground from './images/bg-header-desktop.svg'
+import cardImage from './images/myhome.svg'
 
 function App() {
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-         <img src={HeaderBackground} alt= 'Header image' />
-      </header>
-      <div className='main'>
-        <div className='card'>
-          <img src={cardImage} alt='image' />
-          <div className='description'>
-            <span className='sk-1'>Photosnap</span>
-            <span className='sk-2'>NEW!</span>
-            <span className='sk-3'>FEATURED</span><br/> 
-            <span className='sk-title'>Senior Frontend Developer</span><br/> 
-            <span className='bt'>Id ago</span>
-            <span className='bt'>.</span>
-            <span className='bt'>Full Time</span>
-            <span className='bt'>.</span>
-            <span className='bt'>USA only</span>
+      <div className="App">
+        <header className="App-header">
+          <img src={HeaderBackground} alt="Header image" />
+        </header>
+        <div className="main">
+          <div className="card">
+            <img src={cardImage} alt="image" />
+            <div className="description">
+              <p>
+                <span className="sk-1">Photosnap</span>
+                <span className="sk-2">NEW!</span>
+                <span className="sk-3">FEATURED</span>
+              </p>
+              <p>
+                <span className="sk-title">Senior Frontend Developer</span>
+                <br />
+              </p>
+              <p className="bt">
+                <span className="bt">Id ago</span>
+                <span className="bt">・</span>
+                <span className="bt">Full Time</span>
+                <span className="bt">・</span>
+                <span className="bt">USA only</span>
+              </p>
+            </div>
+            <div className="lang">
+              <span>Frontend</span>
+              <span>Senior</span>
+              <span>HTML</span>
+              <span>CSS</span>
+              <span>JavaScript</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
