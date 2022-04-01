9ccf2e9168a50719314bcda8a1ed7ddfeb2b47ef
Davood Khoshnood <45537769+DavoodKhoshnood@users.noreply.github.com>
2022-04-01 23:44:55 +0100

    Search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import './App.css'
+import React, { useState } from 'react'
+
 import data from './data.json'
-import HeaderBackground from './images/bg-header-desktop.svg'
+import headerBackground from './images/bg-header-desktop.svg'
 import LoadImage from './LoadImage'
 
-console.log(data)
+import './App.css'
+
 function App() {
+  const [filteredData, setFilter] = useState(data)
+  const catClick = (category) => {
+    console.log(category)
+    document.getElementById('searchInput').value = category
+    filter(category)
+  }
+  const filter = (inputValue) => {
+    setFilter(
+      data.filter(
+        (obj) =>
+          obj.company.toLowerCase().includes(inputValue.toLowerCase()) ||
+          obj.languages.includes(inputValue) ||
+          obj.role.includes(inputValue) ||
+          obj.level.includes(inputValue) ||
+          (inputValue === 'FEATURED' ? obj.featured : '') ||
+          (inputValue === 'NEW!' ? obj.new : ''),
+      ),
+    )
+  }
+  const addEvent = () =>
+    document
+      .querySelectorAll('span')
+      .forEach((el) =>
+        el.addEventListener('click', (e) => catClick(e.target.innerText)),
+      )
+
   return (
     <>
       <div className="App">
         <header className="App-header">
-          <img src={HeaderBackground} alt="HeaderImage" />
+          <img src={headerBackground} alt="HeaderImage" />
         </header>
+
         <div className="main">
-          {data.map((job) => (
+          <div className="content">
+            <input
+              id="searchInput"
+              className="form-control"
+              type="search"
+              placeholder="Search..."
+              onChange={(e) => filter(e.target.value)}
+            />
+          </div>
+          {filteredData.map((job) => (
             <div className="card">
               <LoadImage alt={job.alt} imgPath={job.logo} />
               <div className="description">
                 <p>
                   <span className="sk-1">{job.company}</span>
-                  {job.new?<span className="sk-2">NEW!</span>:''}
-                  {job.featured?<span className="sk-3">FEATURED</span>:''}
+                  {job.new ? <span className="sk-2">NEW!</span> : ''}
+                  {job.featured ? <span className="sk-3">FEATURED</span> : ''}
                 </p>
                 <p>
                   <span className="sk-title">{job.position}</span>
@@ -36,16 +74,16 @@ function App() {
               <div className="lang">
                 <span>{job.role}</span>
                 <span>{job.level}</span>
-                {job.languages.map(lan =>
+                {job.languages.map((lan) => (
                   <span>{lan}</span>
-                )}
+                ))}
               </div>
             </div>
           ))}
         </div>
       </div>
+      {addEvent()}
     </>
   )
 }
-
 export default App
--- src/LoadImage.js
+++ src/LoadImage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const LoadImage = (props) => {
-  console.log(props)
   return <img alt='test' src={props.imgPath} />
 }
 
